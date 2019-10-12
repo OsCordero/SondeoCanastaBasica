337c0adea02ba431b0c1419524a9f28297e63883
Oscar Cordero <oscarcordero97@gmail.com>
2019-10-11 22:15:58 -0600

    hola mundo

--- src/main/java/com/too/ues/edu/canastabasica/DemoApplication.java
+++ src/main/java/com/too/ues/edu/canastabasica/DemoApplication.java
@@ -2,12 +2,27 @@ package com.too.ues.edu.canastabasica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+	@GetMapping(value="/hello")
+	public String getMethodName() {
+		return "Hola Mundo";
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
 	}
 
 }
